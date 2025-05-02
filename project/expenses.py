25995ed471f802adf4504906302b4684f8ebe948
Dhwani2503 <25dhwani@gmail.com>
2025-05-02 16:00:09 -0400

    Delete project directory

--- project/expenses.py
+++
@@ -1,6 +0,0 @@
-class Expense:
-    def __init__(self, amount, category):
-        """Expense object representing a financial expense."""
-        self.amount = amount
-        self.category = category
-        self.transaction_type = "Expense"
--- project/income.py
+++
@@ -1,6 +0,0 @@
-class Income:
-    def __init__(self, amount, category):
-        """Income object representing a financial income."""
-        self.amount = amount
-        self.category = category
-        self.transaction_type = "Income"
